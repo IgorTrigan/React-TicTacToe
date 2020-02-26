a546982cc9e4095425d8f92c1db23f9069e47bd9
Igor <42315657+IgorTrigan@users.noreply.github.com>
2020-02-26 03:30:05 +0200

    Create precache-manifest.9ae8be67f7d79a4caa71873d06e3dc6d.js

---
+++ precache-manifest.9ae8be67f7d79a4caa71873d06e3dc6d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcccabbe35490c053cce4e187dce67bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76afe70b96b22eac936f",
+    "url": "/static/css/main.1ff61f8b.chunk.css"
+  },
+  {
+    "revision": "379b9940224e7769031d",
+    "url": "/static/js/2.c99742a8.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.c99742a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76afe70b96b22eac936f",
+    "url": "/static/js/main.29025eff.chunk.js"
+  },
+  {
+    "revision": "252a84562cd0bbdcee23",
+    "url": "/static/js/runtime-main.d720786c.js"
+  },
+  {
+    "revision": "c907543ceec055cbf26e116cc00bf448",
+    "url": "/static/media/cancel.c907543c.svg"
+  },
+  {
+    "revision": "c907543ceec055cbf26e116cc00bf448",
+    "url": "/static/media/cancel1.c907543c.svg"
+  },
+  {
+    "revision": "505d1d71c86ba483be33a806e206f5df",
+    "url": "/static/media/circle.505d1d71.svg"
+  },
+  {
+    "revision": "505d1d71c86ba483be33a806e206f5df",
+    "url": "/static/media/circle1.505d1d71.svg"
+  }
+]);
